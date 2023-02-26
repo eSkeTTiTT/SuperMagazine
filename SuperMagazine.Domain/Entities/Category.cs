5e4113ccbb49b3f982a3c42a11e3beb34fa5d2d3
semyo <semyoshkin8v@mail.ru>
2023-02-26 20:45:10 +0300

    Category and Product

---
+++ SuperMagazine.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperMagazine.Domain.Entities
+{
+	public class Category
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public List<Product> Products { get; set; } = new();
+	}
+}
---
+++ SuperMagazine.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperMagazine.Domain.Entities
+{
+	public class Product
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Key]
+		public int Id { get; set; }
+
+		public int CategoryId { get; set; }
+		public Category Category { get; set; } = null!;
+
+		public string Name { get; set; } = null!;
+
+		public string ImageUrl { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public decimal Price { get; set; }
+	}
+}
