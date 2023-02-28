9e66c04aa9c04ab7e8ef393bbc7582e335c18623
semyo <semyoshkin8v@mail.ru>
2023-02-28 20:33:57 +0300

    ICategoryRepository

---
+++ SuperMagazine.DAL/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using SuperMagazine.Domain.Entities;
+
+namespace SuperMagazine.DAL.Interfaces
+{
+	public interface ICategoryRepository
+	{
+		Task<Category?> GetById(int id);
+		Task<List<Category>> Select();
+	}
+}
---
+++ SuperMagazine.DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using SuperMagazine.DAL.Interfaces;
+using SuperMagazine.Domain.Entities;
+
+namespace SuperMagazine.DAL.Repositories
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		#region Properties
+
+		private readonly ApplicationDbContext _dbContext = null!;
+		private readonly IMemoryCache _memoryCache = null!;
+
+		#endregion
+
+		public CategoryRepository(ApplicationDbContext dbContext, IMemoryCache memoryCache)
+		{
+			_dbContext = dbContext;
+			_memoryCache = memoryCache;
+		}
+
+		public async Task<Category?> GetById(int id) =>
+			await _dbContext.Categories.FirstOrDefaultAsync(v => v.Id == id);
+
+		public async Task<List<Category>> Select() =>
+			await _dbContext.Categories.ToListAsync();
+	}
+}
