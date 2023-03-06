f2f4843588545fcac8de89ec97a55e460998607e
semyo <semyoshkin8v@mail.ru>
2023-03-06 19:30:23 +0300

    bucket view model

---
+++ SuperMagazine.Domain/Models/Catalog/BucketViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperMagazine.Domain.Models.Catalog
+{
+    public class BucketViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
