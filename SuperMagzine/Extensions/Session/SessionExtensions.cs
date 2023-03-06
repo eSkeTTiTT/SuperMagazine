bb5f187447be692b174f7f64b2369e23a054bb45
semyo <semyoshkin8v@mail.ru>
2023-03-06 20:09:44 +0300

    Расширение для сессии - добавления сложных объектов

---
+++ SuperMagzine/Extensions/Session/SessionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace SuperMagazine.Extensions.Session
+{
+    public static class SessionExtension
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize<T>(value));
+        }
+
+        public static T? Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+        }
+    }
+}
