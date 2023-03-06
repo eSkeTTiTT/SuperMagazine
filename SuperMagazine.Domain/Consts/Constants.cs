c5311c61094a709dc95e1d8901e143cd9e7a6f14
semyo <semyoshkin8v@mail.ru>
2023-03-06 19:38:53 +0300

    константы проекта

---
+++ SuperMagazine.Domain/Consts/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperMagazine.Domain.Consts
+{
+    public static class Constants
+    {
+        public static string SESSION_COUNT = "Count";
+        public static string SESSION_BUCKET_LIST = "BucketList";
+    }
+}
